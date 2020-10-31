428090cc99c0cf6e4a908962c719928b9539df77
tyzlddy <tyzlddy@naver.com>
2020-10-31 16:55:44 +0900

    [+] Mod templateScreen page

--- FrontE/FishGo/templateScreen.js
+++ FrontE/FishGo/templateScreen.js
@@ -9,43 +9,26 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
-  Text,
-  Button,
-  TextInput
+  Text
 } from 'react-native';
 
-class SignUpScreen extends Component {
+class ScreenTemplate extends Component {
   render() {
     return (
-      <View style={{
-        backgroundColor: '#2fa9ff',
-        width:'100%',
-        height:'100%',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}>
-        <View style={{
-          padding:30,
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}>
-          <Text style={{
-            fontWeight: 'bold',
-            fontSize:80,
-          }}>Fish~ Go!</Text>
-        </View>
+      <View style={styles.container}>
+        <Text>Hello world</Text>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  main:{
-      backgroundColor: "#ffffff",
-      flex:1,
-          alignItems: 'center',
-          justifyContent: 'center',
+  container:{
+    backgroundColor: "#ffffff",
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
 
-export default SignUpScreen;
+export default ScreenTemplate;
