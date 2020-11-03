25c3edf9a07cf7a0494fcb879e909381c83b3127
tyzlddy <tyzlddy@naver.com>
2020-11-04 02:41:30 +0900

    [+] Mod test dummy data from parent component

--- FrontE/FishGo/src/Collection/collection_detail.js
+++ FrontE/FishGo/src/Collection/collection_detail.js
@@ -9,88 +9,143 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
-  Text,
+  Text, 
   Button,
   Image,
   SafeAreaView,
   ScrollView
 } from 'react-native';
 
-class SignUpScreen extends Component {
+class CollectionDetailScreen extends Component {
     render() {
-        return (
-            <SafeAreaView>
-                <ScrollView>
-            <View style={{
-                // backgroundColor: '#2fa9ff',
-                width:'100%',
-                height:'100%',
-                alignItems: 'center',
-            }}>
-                <View style={{
-                padding:20,
-                alignItems: 'center',
-                }}>
-                <Text style={{
-                    fontWeight: 'bold',
-                    fontSize:40,
-                }}>Fish~ Go!</Text>
-                </View>
-                <Image 
-                    style={{width: 100, height: 100}}
-                    source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-                />
-                <View style={{
-                    elevation: 8,
-                    backgroundColor: '#fff',
-                    flex: 1,
-                    margin: 20,
-                    padding:10,
-                    width:'90%',
-                    height: '100%',
-                    // justifyContent:'center',
-                    alignItems:'center',
-                    borderRadius: 15,
-                }}>
-                    <View style={{
-                        padding: 10,
-                        flexDirection: 'row',
-                        justifyContent: 'space-between'
-                    }}>
-                        <Text style={{fontSize:30}}>물고기 이름</Text>
-                        <Text>물고기 잡은 날짜</Text>
+        const {params} = this.props.route;
+        
+        if(params.check){
+            return (
+                <SafeAreaView>
+                    <ScrollView>
+                        <View style={styles.mainView}>
+                            <View>
+                            <Text style={styles.headerText}>Fish~ Go!</Text>
+                            </View>
+                            <Image 
+                                style={{margin:10, width: 180, height: 180, resizeMode:'contain'}}
+                                source={{uri: params.fish_image}}
+                            />
+                            <View style={styles.mainContentView}>
+                                <View style={styles.subcontentView}>
+                                    <View>
+                                    <Text style={{fontSize:30}}>{params.fish_name}</Text>
+                                    </View>
+                                    <View style={{paddingTop:20, paddingLeft:40}}>
+                                    <Text>{params.uf_date}</Text>
+                                    </View>
+                                </View>
+                                <View style={styles.subcontentView}>
+                                    <View style={{
+                                        flexDirection:"column",
+                                        alignItems: 'center',
+                                    }}>
+                                        <Text>어종 타입</Text>
+                                        <Text>{params.fish_type}</Text>
+                                    </View>
+                                    <View style={{
+                                        flexDirection:"column",
+                                        alignItems: 'center',
+                                        paddingLeft: 20,
+                                        paddingRight: 20
+                                    }}>
+                                        <Text>서식지</Text>
+                                    <Text>{params.fish_home}</Text>
+                                    </View>
+                                    <View style={{
+                                        flexDirection:"column",
+                                        alignItems: 'center'
+                                    }}>
+                                        <Text>길이</Text>
+                                    <Text>{params.uf_length}</Text>
+                                    </View>
+                                </View>
+                                <View style={styles.sub2ContentView}>
+                                    <View style={{
+                                        flexDirection:'row',
+                                        justifyContent:'flex-start'
+                                    }}>
+                                        <Text>먹이 : </Text>
+                                        <Text>{params.fish_feed}</Text>
+                                    </View>
+                                    <View style={{
+                                        flexDirection:'row',
+                                        justifyContent:'flex-start'
+                                    }}>
+                                        <Text>포획 금지 조건 : </Text>
+                                        <Text>{params.fish_prohibition}</Text>
+                                    </View>
+                                    <View style={{
+                                        flexDirection:'row',
+                                        justifyContent:'flex-start'
+                                    }}>
+                                        <Text>조리법 : </Text>
+                                        <Text>{params.fish_receipe}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                            <Button
+                                title="목록으로"
+                                onPress={()=>{
+                                    this.props.navigation.navigate('Collection')
+                                }}
+                            />
+                        </View>
+                    </ScrollView>
+                </SafeAreaView>
+            )
+        }
+        else{
+            return(
+                <SafeAreaView>
+                    <View>
+                        <Text>아직 잡지 못하였어요!</Text>
                     </View>
-                    <View style={{
-                        padding: 10,
-                        flexDirection: 'row',
-                        justifyContent: 'space-between'
-                    }}>
-                        <Text>물고기 타입 / </Text>
-                        <Text>물고기 서식지 / </Text>
-                        <Text>길이</Text>
-                    </View>
-                    
-                </View>
-                <Button
-                    title="목록으로"
-                    onPress={()=>{
-                        this.props.navigation.navigate('Collection')
-                    }}
-                />
-            </View>
-            </ScrollView>
-            </SafeAreaView>
-        )
+                </SafeAreaView>
+            )
+        }
     }
 }
 
 const styles = StyleSheet.create({
-  main:{
-      backgroundColor: "#ffffff",
-      flex:1,
-          alignItems: 'center',
-          justifyContent: 'center',
+  mainView:{
+      // backgroundColor: '#2fa9ff',
+      width:'100%',
+      height:'100%',
+      alignItems: 'center',
+      padding:20,
+  },
+  mainContentView:{
+    elevation: 8,
+    backgroundColor: '#fff',
+    flex: 1,
+    margin: 20,
+    padding:10,
+    width:'90%',
+    height: '100%',
+    alignItems:'center',
+    borderRadius: 15,
+  },
+  subcontentView:{
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerText:{
+    fontWeight: 'bold',
+    fontSize:40,
+  },
+  sub2ContentView: {
+    padding: 10,
+    justifyContent:'flex-start'
+    // justifyContent: 'space-between'
   }
 });
 
-export default SignUpScreen;
+export default CollectionDetailScreen;
