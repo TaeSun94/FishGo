9c69720ddd6a9ea35824699f70357fba146cff54
tyzlddy <tyzlddy@naver.com>
2020-11-04 02:42:22 +0900

    [+] Mod Collection main component

--- FrontE/FishGo/src/Collection/collection.js
+++ FrontE/FishGo/src/Collection/collection.js
@@ -14,327 +14,156 @@ import {
   TextInput,
   TouchableOpacity,
   SafeAreaView,
-  ScrollView
+  ScrollView,
+  Image
 } from 'react-native';
-
-class SignUpScreen extends Component {
+import fish from '../assets/밝음.png';
+import fish_dark from '../assets/암흑.png'
+class Collection extends Component {
   render() {
     return (
       <SafeAreaView style={{
-        backgroundColor: '#2fa9ff',
+        // backgroundColor: '#2fa9ff',
         width:'100%',
         height:'100%',
+        display:'flex',
       }}>
         <ScrollView>
-        <View style={{
-            width:'100%',
+        <View 
+          style={{
+            // width:'100%',
             padding: 10,
             flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
+            justifyContent: 'flex-start',
+            flexWrap:'wrap'
+          }}
+        >
           <TouchableOpacity
           onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          style={styles.btn}
+            this.props.navigation.navigate('Collection_detail',{
+                check: true,
+                fish_name:"아무 물고기",
+                uf_date: "2020-11-03",
+                fish_type: "해수어",
+                fish_home: "동해 어느 바닷가",
+                uf_length: "13.6cm",
+                fish_feed: "지렁이",
+                fish_prohibition: "11cm이하 포획금지",
+                fish_receipe: "회, 찜, 탕",
+                fish_image: "https://reactnative.dev/img/tiny_logo.png"
+              })
+            }}
+            style={styles.btn}
           >
+            <Image source={fish}
+              style={{
+                width:180,
+                height:150,
+                resizeMode: 'contain'
+              }}
+            />
             <Text>물고기</Text>
           </TouchableOpacity>
           <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
+            style={styles.btn}
+            onPress={()=>{
+              this.props.navigation.navigate('Collection_detail',{
+                check:false
+              })
+            }}
           >
+            <Image source={fish_dark}
+              style={{
+                width:180,
+                height:150,
+                resizeMode: 'contain'
+              }}
+            />
             <Text>물고기</Text>
           </TouchableOpacity>
           <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
+            style={styles.btn}
+            onPress={()=>{
+              this.props.navigation.navigate('Collection_detail',{
+                check:false
+              })
+            }}
           >
+            <Image source={fish_dark}
+              style={{
+                width:180,
+                height:150,
+                resizeMode: 'contain'
+              }}
+            />
             <Text>물고기</Text>
           </TouchableOpacity>
           <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
+            style={styles.btn}
+            onPress={()=>{
+              this.props.navigation.navigate('Collection_detail',{
+                check:false
+              })
+            }}
           >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-            width:'100%',
-            padding: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
-          <TouchableOpacity
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          style={styles.btn}
-          >
+            <Image source={fish_dark}
+              style={{
+                width:180,
+                height:150,
+                resizeMode: 'contain'
+              }}
+            />
             <Text>물고기</Text>
           </TouchableOpacity>
           <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
+            style={styles.btn}
+            onPress={()=>{
+              this.props.navigation.navigate('Collection_detail',{
+                check:false
+              })
+            }}
           >
+            <Image source={fish_dark}
+              style={{
+                width:180,
+                height:150,
+                resizeMode: 'contain'
+              }}
+            />
             <Text>물고기</Text>
           </TouchableOpacity>
           <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
+            style={styles.btn}
+            onPress={()=>{
+              this.props.navigation.navigate('Collection_detail',{
+                check:false
+              })
+            }}
           >
+            <Image source={fish_dark}
+              style={{
+                width:180,
+                height:150,
+                resizeMode: 'contain'
+              }}
+            />
             <Text>물고기</Text>
           </TouchableOpacity>
           <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
+            style={styles.btn}
+            onPress={()=>{
+              this.props.navigation.navigate('Collection_detail',{
+                check:false
+              })
+            }}
           >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-            width:'100%',
-            padding: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
-          <TouchableOpacity
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          style={styles.btn}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-            width:'100%',
-            padding: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
-          <TouchableOpacity
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          style={styles.btn}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-            width:'100%',
-            padding: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
-          <TouchableOpacity
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          style={styles.btn}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-            width:'100%',
-            padding: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
-          <TouchableOpacity
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          style={styles.btn}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-            width:'100%',
-            padding: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
-          <TouchableOpacity
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          style={styles.btn}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-            width:'100%',
-            padding: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
-          <TouchableOpacity
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          style={styles.btn}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
-            <Text>물고기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.btn}
-          onPress={()=>{
-            this.props.navigation.navigate('Collection_detail')
-          }}
-          >
+            <Image source={fish_dark}
+              style={{
+                width:180,
+                height:150,
+                resizeMode: 'contain'
+              }}
+            />
             <Text>물고기</Text>
           </TouchableOpacity>
         </View>
@@ -352,8 +181,9 @@ const styles = StyleSheet.create({
           justifyContent: 'center',
   },
   btn:{
-    width: 80,
-    height: 80,
+    width: 180,
+    height: 180,
+    margin: 5,
     borderColor: 'white',
     borderWidth:2,
     marginRight: 10,
@@ -362,4 +192,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default SignUpScreen;
+export default Collection;
