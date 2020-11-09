f8ff5c8bcd5510f4f158886434843bf3b951e395
Kim Ki Young <kykim818@naver.com>
2020-11-09 10:49:11 +0900

    [+] Mod Mobx setting

--- FrontE/FishGo/App.js
+++ FrontE/FishGo/App.js
@@ -24,34 +24,35 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/home';
 import SignUpScreen from './src/signup';
 import LoginScreen from './src/login';
-import Map from './src/map';
 import CollectionDetailScreen from './src/Collection/collection_detail'
 import CollectionScreen from './src/Collection/collection';
+import Map from './src/map';
 import CollectionInsertScreen from './src/Collection/collection_insert';
 import DescriminationScreen from './src/Discrimination/discrimination';
-
 const Stack = createStackNavigator();
 
 class App extends Component{
   render (){
     return (
-      <NavigationContainer>
-        <Stack.Navigator 
-          initialRouteName="Login"
-          screenOptions={{
-            headerShown:false
-          }}
-        >
-          <Stack.Screen name="Home" component={HomeScreen}/>
-          <Stack.Screen name="Signup" component={SignUpScreen}/>
-          <Stack.Screen name="Login" component={LoginScreen}/>
-          <Stack.Screen name="Map" component={Map}/>
-          <Stack.Screen name="Collection_detail" component={CollectionDetailScreen}/>
-          <Stack.Screen name="Collection" component={CollectionScreen}/>
-          <Stack.Screen name="Collection_insert" component={CollectionInsertScreen}/>
-          <Stack.Screen name="Descrimination" component={DescriminationScreen}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Provider {...stores}>
+        <NavigationContainer>
+          <Stack.Navigator 
+            initialRouteName="Login"
+            screenOptions={{
+              headerShown:false
+            }}
+          >
+            <Stack.Screen name="Home" component={HomeScreen}/>
+            <Stack.Screen name="Signup" component={SignUpScreen}/>
+            <Stack.Screen name="Login" component={LoginScreen}/>
+            <Stack.Screen name="Map" component={Map}/>
+            <Stack.Screen name="Collection_detail" component={CollectionDetailScreen}/>
+            <Stack.Screen name="Collection" component={CollectionScreen}/>
+            <Stack.Screen name="Collection_insert" component={CollectionInsertScreen}/>
+            <Stack.Screen name="Descrimination" component={DescriminationScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
     )
   }
 };
